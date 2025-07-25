79b6f785dd799124f21f6650e09db16f38c10486
Nolan Nordlund <nolan@kirschbaumdevelopment.com>
2025-07-25 16:24:33 -0500

    Updating for latest filament-icon changes

--- src/Enums/PhosphorStyle.php
+++ src/Enums/PhosphorStyle.php
@@ -2,7 +2,9 @@
 
 namespace Filafly\Icons\Phosphor\Enums;
 
-enum PhosphorStyle: string
+use Filafly\Icons\Contracts\StyleEnum;
+
+enum PhosphorStyle: string implements StyleEnum
 {
     case Regular = '';
     case Bold = '-bold';
@@ -10,4 +12,46 @@ enum PhosphorStyle: string
     case Fill = '-fill';
     case Light = '-light';
     case Thin = '-thin';
+
+    public function getStyleName(): string
+    {
+        return match ($this) {
+            self::Regular => 'regular',
+            self::Bold => 'bold',
+            self::Duotone => 'duotone',
+            self::Fill => 'fill',
+            self::Light => 'light',
+            self::Thin => 'thin',
+        };
+    }
+
+    public function getEnumSuffix(): string
+    {
+        return match ($this) {
+            self::Regular => 'Regular',
+            self::Bold => 'Bold',
+            self::Duotone => 'Duotone',
+            self::Fill => 'Fill',
+            self::Light => 'Light',
+            self::Thin => 'Thin',
+        };
+    }
+
+    public static function getStyleNames(): array
+    {
+        return ['regular', 'bold', 'duotone', 'fill', 'light', 'thin'];
+    }
+
+    public static function fromStyleName(string $styleName): ?self
+    {
+        return match (strtolower($styleName)) {
+            'regular' => self::Regular,
+            'bold' => self::Bold,
+            'duotone' => self::Duotone,
+            'fill' => self::Fill,
+            'light' => self::Light,
+            'thin' => self::Thin,
+            default => null,
+        };
+    }
 }
